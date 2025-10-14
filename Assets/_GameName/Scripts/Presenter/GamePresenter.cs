d8f1da9d1ca0a531b0e06aced7ef66d08c1cf60c
Hardik Hapaliya <hardik.hapaliya.patel@gmail.com>
2025-10-14 23:39:20 +0200

    chore: add ScriptableObject assets for cards & layout and add TextMeshPro plugin

--- Assets/_GameName/Scripts/Presenter/GamePresenter.cs
+++ Assets/_GameName/Scripts/Presenter/GamePresenter.cs
@@ -61,11 +61,12 @@ public class GamePresenter : IDisposable
         for (int i = 0; i < cardModels.Count; i++)
         {
             var cardView = boardView.CreateCard();
+            var cardModel = cardModels[i];
             var cardData = cardDatas.First(d => d.id == cardModels[i].id);
             var cardPresenter = new CardPresenter(cardModels[i], cardView, cardData.frontSprite);
 
             // Connect clicks
-            cardView.OnClicked.Subscribe(_ => OnCardClicked(cardModels[i])).AddTo(disposables);
+            cardView.OnClicked.Subscribe(_ => OnCardClicked(cardModel)).AddTo(disposables);
             presenters.Add(cardPresenter);
         }
 
@@ -74,6 +75,7 @@ public class GamePresenter : IDisposable
 
     private void OnCardClicked(CardModel cardModel)
     {
+        Debug.Log("OnCardClicked");
         if (!cardModel.IsInteractable.Value || cardModel.State.Value != CardState.FaceDown) return;
 
         cardModel.SetState(CardState.FaceUp);
